0462202bd40ebef5fb9e98310b75ced010db5684
ATJP88 <93024157+ATJP88@users.noreply.github.com>
2022-12-09 13:38:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2_example" {
 
-   ami           = "ami-0767046d1677be5a0"
+   ami           = "ami-0b0dcb5067f052a63"
    instance_type =  var.instance_type
 
   tags = {
